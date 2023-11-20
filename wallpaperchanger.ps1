b83b003e70070145ab71b00bc9342589e5337c90
Max Gernhoefer <maximiliang60@gmail.com>
2023-11-20 21:08:27 +1300

    Now it retrys on 404

--- wallpaperchanger.ps1
+++ wallpaperchanger.ps1
@@ -6,83 +6,87 @@ $liSelector = "li"
 
 # CSS selector for the section
 $sectionSelector = 'section class="thumb-listing-page"'
-$scriptPath = $MyInvocation.MyCommand.Path
 $imagePath = "$env:TEMP\wallpaper.jpg"
 
-# Download the HTML content of the website
-$html = Invoke-WebRequest -Uri $url
+$failed = 0
 
-# Find the section with the specified class
-$sectionTag = $html.RawContent -match "<$sectionSelector>(.*?)</section>"
+function DownloadImage(){
+    # Download the HTML content of the website
+    $html = Invoke-WebRequest -Uri $url
 
-# Check if a section tag was found
-if ($Matches.Count -gt 0) {
-    # Find all unordered lists within the section
-    $ulTags = $Matches[0] -split "<ul[^>]*>"
+    # Find the section with the specified class
+    $sectionTag = $html.RawContent -match "<$sectionSelector>(.*?)</section>"
 
-    # Check if unordered lists were found
-    if ($ulTags.Count -gt 1) {
-        # Extract the content of the first list item within the first unordered list
-        $firstListItemContent = $ulTags[1] -match "<$liSelector.*?>(.*?)</$liSelector>"
+    # Check if a section tag was found
+    if ($Matches.Count -gt 0) {
+        # Find all unordered lists within the section
+        $ulTags = $Matches[0] -split "<ul[^>]*>"
 
-        # Check if a list item was found
-        if ($Matches.Count -gt 0) {
-            # Extract the text content of the first list item
-            $firstListItemText = $Matches[1].Trim()
+        # Check if unordered lists were found
+        if ($ulTags.Count -gt 1) {
+            # Extract the content of the first list item within the first unordered list
+            $firstListItemContent = $ulTags[1] -match "<$liSelector.*?>(.*?)</$liSelector>"
 
-            # Save the text content to a file
-            $firstListItemText | Out-File -FilePath $outputPath -Encoding UTF8
-            Write-Host "Text content of the first list item saved successfully."
-        } else {
-            Write-Host "List item not found based on the provided list item selector."
+            # Check if a list item was found
+            if ($Matches.Count -gt 0) {
+                # Extract the text content of the first list item
+                $firstListItemText = $Matches[1].Trim()
+                Write-Host "Found section"
+            }
+            else {
+                Write-Host "List item not found based on the provided list item selector."
+            }
         }
-    } else {
-        Write-Host "No unordered lists found within the section."
+        else {
+            Write-Host "No unordered lists found within the section."
+        }
     }
-} else {
-    Write-Host "Section with class '$sectionSelector' not found on the page."
-}
+    else {
+        Write-Host "Section with class '$sectionSelector' not found on the page."
+    }
 
 
-#when the url is https://wallhaven.cc/w/dpovpm for example
-#the full image will be https://w.wallhaven.cc/full/dp/wallhaven-dpovpm.jpg
-# see how the dp part is is split
+    #when the url is https://wallhaven.cc/w/dpovpm for example
+    #the full image will be https://w.wallhaven.cc/full/dp/wallhaven-dpovpm.jpg
+    # see how the dp part is is split
 
-# Use regular expression to extract the href value
-$hrefPattern = 'href="([^"]+)"'
-$match = [regex]::Match($firstListItemText, $hrefPattern)
+    # Use regular expression to extract the href value
+    $hrefPattern = 'href="([^"]+)"'
+    $match = [regex]::Match($firstListItemText, $hrefPattern)
 
-# Check if a match is found
-if ($match.Success) {
-    $hrefValue = $match.Groups[1].Value
-    Write-Output $hrefValue
-} else {
-    Write-Output "Href not found in the input string."
-}
+    # Check if a match is found
+    if ($match.Success) {
+        $hrefValue = $match.Groups[1].Value
+        # Write-Output $hrefValue
+    }
+    else {
+        Write-Output "Href not found in the input string."
+    }
 
-#we only care about the last 6 characters of the url
-$hrefValue = $hrefValue[-6..-1] -join ''
-$firstTwoChars = $hrefValue.Substring(0, 2)
-$newUrl = "https://w.wallhaven.cc/full/${firstTwoChars}/wallhaven-${hrefValue}.jpg"
-
+    #we only care about the last 6 characters of the url
+    $hrefValue = $hrefValue[-6..-1] -join ''
+    $firstTwoChars = $hrefValue.Substring(0, 2)
+    $newUrl = "https://w.wallhaven.cc/full/${firstTwoChars}/wallhaven-${hrefValue}.jpg"
+    
+    Write-Output "Url is ${newUrl}"
+    Write-Output "Downloading..."
     # Use Invoke-WebRequest to download the image
     try {
         Invoke-WebRequest -Uri $newUrl -OutFile $imagePath
+        $failed = 0
     }
     catch {
 
         Write-Host "Image not found. Restarting the script..."
-
-        # Restart the script
-        & "$scriptPath"
-        exit
+        $failed = 1
+        DownloadImage
     }
 
-Write-Output $newUrl
+}
 
-
-# Set the desktop background
-Add-Type -TypeDefinition @"
+    function SetImage() {
+        # Set the desktop background
+        Add-Type -TypeDefinition @"
     using System;
     using System.Runtime.InteropServices;
     public class Wallpaper {
@@ -91,10 +95,22 @@ Add-Type -TypeDefinition @"
     }
 "@
 
-# Constants for SystemParametersInfo
-$SPI_SETDESKWALLPAPER = 0x0014
-$SPIF_UPDATEINIFILE = 0x01
-$SPIF_SENDCHANGE = 0x02
+        # Constants for SystemParametersInfo
+        $SPI_SETDESKWALLPAPER = 0x0014
+        $SPIF_UPDATEINIFILE = 0x01
+        $SPIF_SENDCHANGE = 0x02
 
-# Set the desktop background
-[Wallpaper]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $imagePath, $SPIF_UPDATEINIFILE -bor $SPIF_SENDCHANGE)
+        # Set the desktop background
+        [Wallpaper]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $imagePath, $SPIF_UPDATEINIFILE -bor $SPIF_SENDCHANGE)
+    }
+
+
+
+#main
+DownloadImage
+
+Write-Output "Setting as wallpaper..."
+SetImage
+
+Write-Output "Done.. Press enter to close"
+Read-Host 
